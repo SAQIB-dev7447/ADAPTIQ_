91d8a6bd32c181184417cccdcacfbe021c8ba2d5
arnavce12 <arnav21patil@gmail.com>
2026-03-07 10:36:37 +0530

    chore: add gunicorn, fix app entry point for Render deployment

--- AdaptIQ_Website/app.py
+++ AdaptIQ_Website/app.py
@@ -122,5 +122,6 @@ def feature_ai_detection():
     return render_template("features/ai_detection.html", access_token=session.get('access_token', ''))
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    import os
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
